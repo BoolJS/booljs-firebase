68fd47379dbe63c7c60cfb9d470883b05fdffbcd
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-18 19:46:05 -0500

    Applying models to match caller

--- example/models/dog.js
+++ example/models/dog.js
@@ -2,16 +2,5 @@
 
 var dogs = [];
 
-module.exports = function (app, Schema, mongoose) {
-
-    var DogSchema = new Schema({
-        name: String,
-        age: Number
-    });
-
-    DogSchema.statics.list = function () {
-        return q.nbind(this.find, this)();
-    };
-
-    return DogSchema;
+module.exports = function (app, firebaseRef) {
 };
