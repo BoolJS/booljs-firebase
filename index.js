9594e54830f57f36eb601485b65827a35ef873d1
Pablo Andres Dorado Suarez <im@pandres95.me>
2016-05-10 09:42:35 -0500

    Version bump

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var API = require('bool.js/api');
+const API = require('booljs-api')
+,     lib = require('./lib');
 
-module.exports = new API.DatabaseLoader(
-    'booljs-firebase', // Name
-    require('./lib') // Functions
-);
+module.exports = class BoolJSFirebase extends API.DatabaseLoader {
+    constructor(){
+        super('booljs-firebase');
+    }
+
+    openDatabase(configuration){
+        return lib.openDatabase(configuration);
+    }
+
+    fetchModels(_instance, models, connection){
+        return lib.fetchModels(_instance, models, connection);
+    }
+};
